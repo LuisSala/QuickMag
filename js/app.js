b1c074a888b8a9b8fa33777f0719f4b1fe502190
Luis Sala <lsala@primogen.com>
2011-12-28 14:47:30 -0800

    Basic working app retrieves content from an Alfresco server and displays on-screen;

--- js/app.js
+++ js/app.js
@@ -1,4 +1,10 @@
-var App = Em.Application.create();
+var App = Em.Application.create({
+
+    ready: function() {
+        App.itemsController.loadItems();
+    }
+
+});
 
 App.CONFIG = {
     alfresco: {
@@ -20,34 +26,75 @@ App.CONFIG = {
 
 /*
  * Datastore and Model Declarations
- *
+ * Eventually, we'll use Ember Data, but it doesn't currently support "hasOne" relationships.
  */
 App.models = {};
 
-// The following is not required until DS.hasOne is implemented.
-/*
-App.models.Property = DS.Model.extend({
-    name: DS.attr('string',{key: 'cm:name'}),
-    title: DS.attr('string',{key: 'cm:title'}),
-    description: DS.attr('string', {key: 'cm:description'}),
-    contentURL: DS.attr('string'),
+App.models.Item = Em.Object.extend({
+    data: {}, // Raw data object used to populate model instance.
+
+    id: function() {
+        return this.get('data').node.nodeRef;
+    }.property('data').cacheable(),
+
     dateValue: function() {
-        return this.get('cm:modified').value;
-    }.property().cacheable()
+        return this.get('data').node.properties['cm:modified']['value'];
+    }.property('data').cacheable(),
+
+    name: function() {
+        return this.get('data').node.properties['cm:name'];
+    }.property('data').cacheable(),
+
+    title: function() {
+        return this.get('data').node.properties['cm:title'];
+    }.property('data').cacheable(),
+
+    description: function() {
+        return this.get('data').node.properties['cm:description'];
+    }.property('data').cacheable(),
+
+    _content: "",
+
+    contentUrl: function() {
+        return this.get('data').node.contentURL;
+    }.property('data').cacheable(),
+
+    contentBody: function() {
+        if (this.get('_content') === "") {
+            var alf = App.CONFIG.alfresco;
+            var proxy = App.CONFIG.proxy;
+            var prefix = proxy.enabled ? proxy.endpoint : '';
+
+            var login = (alf.username && alf.password) ? alf.username + ':' + alf.password + '@' : '';
+
+            var contentUrl = this.get('contentUrl');
+
+            var url = prefix + alf.protocol + '://' + login + alf.host + ':' + alf.port + alf.service_base_path + contentUrl;
+
+            var _self = this;
+
+            $.ajax(url,{
+                username: alf.username,
+                password: alf.password,
+                dataType: 'html',
+                success: function(data){
+                    _self.set('_content', data);
+                }
+            });
+        }
+
+        return this.get('_content');
+    }.property('_content', 'data').cacheable()
+
 });
 
-App.models.Node = DS.Model.extend({
-    primaryKey: 'nodeRef',
-    properties: DS.hasMany(App.models.Property, {embedded: true})
-});
-*/
+// An ArrayController subclasses ArrayProxy but doesn't actually add any functionality. Used here to mark a controller.
+App.itemsController  = Em.ArrayController.create({
 
-App.models.Item = DS.Model.extend({
-    primaryKey: 'webdavUrl'
-});
+    content: [],
 
-App.adapter = DS.Adapter.create({
-    findQuery: function(store, type, query, modelArray) {
+    loadItems: function() {
+
         var alf = App.CONFIG.alfresco;
         var proxy = App.CONFIG.proxy;
         var prefix = proxy.enabled ? proxy.endpoint : '';
@@ -57,17 +104,34 @@ App.adapter = DS.Adapter.create({
         var url = prefix + alf.protocol + '://' + login + alf.host + ':' + alf.port + alf.service_base_path + '/slingshot/doclib2/doclist/' + alf.content_model + '/site/' + alf.site_name +
             "/documentLibrary/" + alf.content_folder;
 
+        var _self = this;
+
+        $.ajax(url,{
+            username: alf.username,
+            password: alf.password,
+            dataType: 'json',
+            success: function(data){
+
+                console.log(data.items);
+
+                for (var i=0; i< data.items.length; i++) {
+                    _self.pushObject(App.models.Item.create({data: data.items[i]}));
+                }
+            }
+        });
+        /*
         $.getJSON(url, function(data){
             console.log(data.items);
-            modelArray.load(data.items);
+
+            for (var item in data.items) {
+                _self.pushObject(App.models.Item.create({data:item}));
+            }
         });
+        */
+    } // end find()
 
-    }
 });
 
-App.store = DS.Store.create({
-    adapter: 'App.adapter'
-});
 
 /*
  * Views
